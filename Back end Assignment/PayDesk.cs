c0a96946e91dd5634ee98bc73dd090e80396fb70
Aulteric <aulteric@abv.bg>
2019-06-17 22:17:40 +0300

    PayDesk Added and Card Classes Finished

---
+++ Back end Assignment/PayDesk.cs
@@ -0,0 +1,47 @@
+﻿using Back_end_Assignment.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Back_end_Assignment
+{
+    class PayDesk
+    {
+        private double purchaseValue;
+
+        public double PurchaseValue
+        {
+            get { return purchaseValue; }
+            set { purchaseValue = value; }
+        }
+        private DiscountCard card;
+
+        public DiscountCard Card
+        {
+            get { return card; }
+            set { card = value; }
+        }
+        public PayDesk(double PurchaseValue, double Turnover, int TypeOfCard)
+        {
+            this.PurchaseValue = PurchaseValue;
+            switch (TypeOfCard)
+            {
+                case 1:
+                    this.card = new BronzeCard(Turnover);
+                    break;
+                case 2:
+                    this.card = new SilverCard(Turnover);
+                    break;
+                case 3:
+                    this.card = new GoldCard(Turnover);
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void GetDiscount()
+        {
+            
+        }
+    }
+}
